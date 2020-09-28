2da3395a516a566b8ca2a9201cde18c18c7d7513
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2020-09-28 12:12:09 -0400

    try to make a switch for default DPMJet A>4 momentum distribution

--- BeAGLE/src/dpmjet3.0-5F-new.f
+++ BeAGLE/src/dpmjet3.0-5F-new.f
@@ -5509,13 +5509,20 @@ C     already samples high momentum for deuteron.
          ! Use IFMDIST, 3rd varaible in control card of FERMI, to switch between
          ! different k momentum distributions
 
-         IF ((NMASS.GE.2) .AND. (NMASS.LE.4) .AND. (IFMDIST.GE.1) ) THEN
-            CALL DT_KFERMI(PABS,NMASS,IFMDIST)
-         ELSE IF ( (NMASS.GT.4) .AND. ((IFMDIST .EQ. -1)) ) THEN
+         IF( IFMDIST .EQ. -1 ) THEN
             CALL DT_DFERMIO(PABS,NMASS)
-         ELSE IF ( (NMASS.GT.4) .AND. ((IFMDIST .NE. -1)) ) THEN
+         ELSE IF ( (NMASS.GE.2) .AND. (NMASS.LE.4) .AND. (IFMDIST.GE.1) ) THEN
+            CALL DT_KFERMI(PABS,NMASS,IFMDIST)
+         ELSE IF ( (NMASS.GT.4) .AND. ((IFMDIST .GE. 1)) ) THEN
             CALL DT_DFERMI(PABS,NMASS)
          ENDIF
+         ! IF ((NMASS.GE.2) .AND. (NMASS.LE.4) .AND. (IFMDIST.GE.1) ) THEN
+         !    CALL DT_KFERMI(PABS,NMASS,IFMDIST)
+         ! ELSE IF ( (NMASS.GT.4) .AND. ((IFMDIST .EQ. -1)) ) THEN
+         !    CALL DT_DFERMIO(PABS,NMASS)
+         ! ELSE IF ( (NMASS.GT.4) .AND. ((IFMDIST .NE. -1)) ) THEN
+         !    CALL DT_DFERMI(PABS,NMASS)
+         ! ENDIF
          PABS = PFERM*PABS
 C        IF (PABS.GE.PBIND) THEN
 C           ILOOP = ILOOP+1
